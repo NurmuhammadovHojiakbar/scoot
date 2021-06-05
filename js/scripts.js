c0371376576812735ed57ff5870da0ef56035ece
NurmuhammadovHojiakbar <pythoncoderuz@gmail.com>
2021-06-05 14:28:33 +0500

    faqs responsive

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,18 @@ button.onclick=function(){
     button.classList.toggle("toggle-off")
     navnav.classList.toggle("wrapper-nav-shown")
 }
+
+
+var faqbutton = document.getElementsByClassName("question-button");
+var i;
+
+for (i = 0; i < faqbutton.length; i++) {
+    faqbutton[i].addEventListener("click", function() {
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+        panel.style.display = "none";
+    } else {
+        panel.style.display = "block";
+    }
+    });
+}
